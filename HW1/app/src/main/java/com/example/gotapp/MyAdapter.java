333d28f267198c2bc1c5ac30b49665d09c076483
Shai Brown <shaibrown95@gmail.com>
2020-11-21 15:14:59 +0200

    stark button now shows the house members

---
+++ HW1/app/src/main/java/com/example/gotapp/MyAdapter.java
@@ -0,0 +1,56 @@
+package com.example.gotapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+import org.w3c.dom.Text;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    private String[] names;
+    private int images[];
+    Context context;
+
+    public MyAdapter(Context ct, String name[], int img[]){
+        context = ct;
+        names = name;
+        images = img;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.mTextView.setText(names[position]);
+        holder.mImageView.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return names.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        private TextView mTextView;
+        private ImageView mImageView;
+
+        public MyViewHolder(@NonNull View view){
+            super(view);
+            mTextView = view.findViewById(R.id.memberName);
+            mImageView = view.findViewById(R.id.picture);
+        }
+    }
+}
--- HW1/app/src/main/java/com/example/gotapp/StarkHouse.java
+++ HW1/app/src/main/java/com/example/gotapp/StarkHouse.java
@@ -8,12 +8,21 @@ import android.os.Bundle;
 
 public class StarkHouse extends AppCompatActivity {
 
+    String houseMemberNames[];
+    int images[] ={R.drawable.eddard, R.drawable.catelyn, R.drawable.robb, R.drawable.sansa, R.drawable.bran, R.drawable.arya,
+            R.drawable.rickon, R.drawable.jon};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starkhouse);
 
+
+        houseMemberNames = getResources().getStringArray(R.array.stark_house);
         RecyclerView rView = (RecyclerView)findViewById(R.id.slist);
+
+        MyAdapter myAdapter = new MyAdapter(this, houseMemberNames, images);
+        rView.setAdapter(myAdapter);
         rView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
