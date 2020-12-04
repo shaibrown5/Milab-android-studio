26fa94bc307c1e72a5030b93c6a165e2bd6ac228
Shai Brown <shaibrown95@gmail.com>
2020-12-04 16:55:04 +0200

    added channel

--- HW2/app/src/main/java/com/example/hw2/MyBroadcast.java
+++ HW2/app/src/main/java/com/example/hw2/MyBroadcast.java
@@ -1,10 +1,12 @@
 package com.example.hw2;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
@@ -14,13 +16,26 @@ public class MyBroadcast extends BroadcastReceiver {
 
         // get quotes
         String[] quote = Quotes.getQuote();
-
+        createNotificationChannel(context);
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "C1")
                 .setPriority(NotificationCompat.PRIORITY_MAX)
                 .setContentTitle(quote[0])
                 .setContentText(quote[1]);
 
         notificationManager.notify(1, builder.build());
+    }
+
+    private void createNotificationChannel(Context context) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel
+                    (new NotificationChannel("C1",
+                            "C1", NotificationManager.IMPORTANCE_HIGH));
+        }
     }
 }
