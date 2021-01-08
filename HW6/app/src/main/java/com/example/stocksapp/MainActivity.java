c6e5696d17413e71f6bfc79be6868c46fb436e73
Shai Brown <shaibrown95@gmail.com>
2021-01-08 18:50:55 +0200

    finished functionality of app

--- HW6/app/src/main/java/com/example/stocksapp/MainActivity.java
+++ HW6/app/src/main/java/com/example/stocksapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.stocksapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    String stockName;
+    EditText stockNameInput;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        stockNameInput = (EditText)findViewById(R.id.userInput);
+        submitButton = (Button)findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                stockName = stockNameInput.getText().toString();
+            }
+        });
     }
 }
